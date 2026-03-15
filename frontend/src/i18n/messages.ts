2d936cfd19fa9f1d6a5128d2251390d1a35b2e24
root <root@pve-1-az-ge>
2026-03-15 19:30:35 +0300

    chore(frontend): drop dead legacy shell surfaces

--- frontend/src/i18n/messages.ts
+++ frontend/src/i18n/messages.ts
@@ -3,7 +3,6 @@ import errorsEn from './locales/en/errors'
 import graphEn from './locales/en/graph'
 import apiEn from './locales/en/api'
 import flowEn from './locales/en/flow'
-import onboardingEn from './locales/en/onboarding'
 import providersEn from './locales/en/providers'
 import shellEn from './locales/en/shell'
 import commonRu from './locales/ru/common'
@@ -11,7 +10,6 @@ import errorsRu from './locales/ru/errors'
 import graphRu from './locales/ru/graph'
 import apiRu from './locales/ru/api'
 import flowRu from './locales/ru/flow'
-import onboardingRu from './locales/ru/onboarding'
 import providersRu from './locales/ru/providers'
 import shellRu from './locales/ru/shell'
 
@@ -22,7 +20,6 @@ export const messages = {
     graph: graphEn,
     api: apiEn,
     flow: flowEn,
-    onboarding: onboardingEn,
     providers: providersEn,
     shell: shellEn,
   },
@@ -32,7 +29,6 @@ export const messages = {
     graph: graphRu,
     api: apiRu,
     flow: flowRu,
-    onboarding: onboardingRu,
     providers: providersRu,
     shell: shellRu,
   },
